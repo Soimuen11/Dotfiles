aa7c76a8f00f1a9b384bc8bbeacfd5cd5db2314e
Soimuen11 <soimuen11@gmail.com>
2020-12-09 11:14:02 +0100

    added new script to automate updating process of dotfiles

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This bash script is yet to be written
+# It should:
+	# delete all the dotfiles in the Dotfiles/ directory
+	# make a copy of all the dotfiles currently in use to Dotfiles/
+	# git add & commit the update
+	# notify me that there has been some change so that I can push the latest
+		# commits to github with a CRON JOB every week
