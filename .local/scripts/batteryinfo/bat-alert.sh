78986d3f48970fd2de49095851298f8ecd1b351b
Soimuen11 <soimuen11@gmail.com>
2020-12-10 11:41:04 +0100

    battery alert improvement
    1. I modified the CRON job so that the notification works properly.
        -> I had to add: export DISPLAY=0.0
    2. I added a bunch of lines so that my script recognizes if my battery
    is plugged in or not. This way you get no alert when you battery is
    already charging.

--- .local/scripts/batteryinfo/bat-alert.sh
+++ .local/scripts/batteryinfo/bat-alert.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
+# Add it as a cron job: 
+#*/1 * * * * export DISPLAY=:0.0 && bash /home/philwayne/Dotfiles/.local/scripts/batteryinfo/bat-alert.sh
+
+ac_adapter=$(acpi -a | cut -d' ' -f3 | cut -d- -f1)
+
 for battery in /sys/class/power_supply/BAT?
 do
 	capacity=$(cat $battery/capacity)
 done
 
 echo $capacity 
-if [[ $capacity -lt 15 ]];
+if [[ "$ac_adapter" = "off" ]] && [[ $capacity -lt 15 ]]; 
 then
-	# aplay r2d2-alarm.wav
-	notify-send 'Warning: Low Battery Power!' && espeak 'Warning: Low Battery Power!'
+		# aplay r2d2-alarm.wav
+		notify-send 'Warning: Low Battery Power!' && espeak 'Warning: Low Battery Power!'
+else
+	:
 fi
 
 
