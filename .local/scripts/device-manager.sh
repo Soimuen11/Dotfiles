34cd57563dfca3adc19f91d0f85c8a28c5d34f24
Soimuen11 <soimuen11@gmail.com>
2021-03-17 12:06:25 +0100

    tweaked dmenu launchers for games and devices
    added a prompt to dmenu launchers

--- .local/scripts/device-manager.sh
+++ .local/scripts/device-manager.sh
@@ -4,15 +4,15 @@
 # necessary lines in my /etc/fstab so that I can access devices as a user
 
 # Choose MOUNT or UNMOUNT mode
-MODE=$(echo -e 'Mount\nUnmount' | dmenu)
+MODE=$(echo -e 'Mount\nUnmount' | dmenu -p "Action:")
 case "$MODE" in
 	Mount) 
 		# Choose device you wish to mount && mount point
-		DEVICE=$(ls /dev/sd* | dmenu)
-		MOUNT_POINT=$(ls /mnt/| dmenu)
+		DEVICE=$(ls /dev/sd* | dmenu -p "Devices:")
+		MOUNT_POINT=$(ls /mnt/| dmenu -p "Mount point:")
 		st -e sudo mount "$DEVICE" /mnt/"$MOUNT_POINT";;
 	Unmount) 
 		# Choose which mounted dev you want unmounted
-		BUSY_DEV=$(mount | awk '{print $3}' | grep 'mnt' | dmenu)
+		BUSY_DEV=$(mount | awk '{print $3}' | grep 'mnt' | dmenu -p "Devices:")
 		st -e sudo umount "$BUSY_DEV";;
 esac
--- .local/scripts/game_launcher.sh
+++ .local/scripts/game_launcher.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # ask me if I want to play: psp / gamecube / gba / nds
-LIBRARY=$(ls "/home/$USER/Games" | dmenu)
+LIBRARY=$(ls "/home/$USER/Games" | dmenu -p "Emulator:")
 
 # show me the appropriate library of games after above choice
-GAME=$(ls "/home/$USER/Games/$LIBRARY" | dmenu)
+GAME=$(ls "/home/$USER/Games/$LIBRARY" | dmenu -p "Game To Launch:")
 GAME_PATH="/home/$USER/Games/$LIBRARY"
 
 case $LIBRARY in
